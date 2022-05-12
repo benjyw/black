b179f05ebeaa616b4463736a51a7825e7ae9ebd9
Benjy Weinberger <benjyw@gmail.com>
2022-05-11 19:16:03 -0700

    Pass flake8

--- src/black_version.py
+++ src/black_version.py
@@ -2,4 +2,4 @@
 A canonical location for importing version (e.g., `from black_version import version`)
 that can trigger generation of the underlying _black_version.py as needed.
 """
-from _black_version import version
+from _black_version import version  # noqa: F401
